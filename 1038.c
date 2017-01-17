7e7087db2ab98f82ecb3c3e6eed16b17ce87aa2b
MattheusSantana <matheusess@hotmail.com>
2017-01-17 12:46:00 -0200

    1038 - Lanche

---
+++ 1038.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main (){
+	int c, q;
+
+	scanf("%d %d", &c, &q);
+	
+	if(c == 1 )
+		printf("Total: R$ %.2lf\n", (q * 4.0));		
+	if(c == 2)
+		
+		printf("Total: R$ %.2lf\n", (q * 4.50));
+	if(c == 3)
+		
+		printf("Total: R$ %.2lf\n", (q * 5.0));
+	if(c == 4)	
+		
+		printf("Total: R$ %.2lf\n", (q * 2.0));
+	if(c==5)
+		printf("Total: R$ %.2lf\n", (q * 1.50));
+		
+	return 0; 	
+}
