b1f54d4cc1bc0215434dffbd6bc5cd96d2890667
MattheusSantana <matheusess@hotmail.com>
2017-01-24 23:17:29 -0200

    1865 - Mjolnir

---
+++ 1865.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	int f, l;
+	char nome;
+
+
+	scanf("%d", &l);
+
+	while(l--){
+		scanf("%s %d", &nome, &f);
+
+		if(strcmp(nome, "Thor" == 0)	
+			printf("Y\n");
+		else
+			printf("N\n");
+	return 0;			
+}
